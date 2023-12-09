5b19dd31f1043aac2d237e7645b8fb37400adc1c
Anthony Roussos <79643636+anthonyrouss@users.noreply.github.com>
2023-12-09 14:22:28 +0200

    Refactor solution for Day 01 - Part 1 - Year 2023
    Refactor code by making an inner class for better structure

--- src/main/java/com/anthonyrouss/adventofcode/year2023/Day01Solver.java
+++ src/main/java/com/anthonyrouss/adventofcode/year2023/Day01Solver.java
@@ -7,63 +7,81 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Day01Solver implements DaySolver {
 
-    private static String FILE_PATH = "year2023/day01/data.txt";
-    private int sum, firstDigitPointer, lastDigitPointer;
+    private static final String FILE_PATH = "year2023/day01/data.txt";
+    private int sum;
+
+    private class Line {
+
+        private final String text;
+        private int calibrationValue, firstDigitPointer, lastDigitPointer;
+
+        protected Line(String text) {
+            this.firstDigitPointer = -1;
+            this.lastDigitPointer = -1;
+            this.text = text;
+        }
+
+        protected void calibratePartOne() {
+
+            // Loop through each char from left to right to find the first digit
+            for (int i = 0; i < text.length(); i++) {
+                if (Character.isDigit(text.charAt(i))) {
+                    firstDigitPointer = i;
+                    break;
+                }
+            }
+
+            if (firstDigitPointer == -1) {
+                throw new RuntimeException(String.format("Line '%s' does not contain any digits.", text));
+            }
+
+            // Loop through each char from right to left to find the last digit
+            for (int j = text.length() - 1; j >= firstDigitPointer; j--) {
+                if (Character.isDigit(text.charAt(j))) {
+                    lastDigitPointer = j;
+                    break;
+                }
+            }
+
+            // Combine the two digits to form a two-digit number
+            calibrationValue = Integer.parseInt(String.valueOf(text.charAt(firstDigitPointer)) + text.charAt(lastDigitPointer));
+
+        }
+
+        protected int getCalibrationValue() {
+            return calibrationValue;
+        }
+    }
 
     @Override
     public int solvePartOne() {
         sum = 0;
 
         try {
-            // Fetch the lines from the data text file
-            List<String> lines = fetchLines();
+            // Fetch the text of the lines from the data text file
+            List<String> linesText = fetchLinesText();
 
             // Loop through each line
-            lines.forEach(line -> {
+            linesText.forEach(lineText -> {
 
-                firstDigitPointer = -1;
-                lastDigitPointer = -1;
+                Line line = new Line(lineText);
 
-                // Loop through each char from left to right to find the first digit
-                for (int i = 0; i < line.length(); i++) {
-                    if (Character.isDigit(line.charAt(i))) {
-                        firstDigitPointer = i;
-                        break;
-                    }
-                }
+                // Calibrate the line to find the two-digit calibration value
+                line.calibratePartOne();
 
-                if (firstDigitPointer == -1) {
-                    throw new RuntimeException(String.format("Line '%s' does not contain any digits.", line));
-                }
-
-                // Loop through each char from right to left to find the last digit
-                for (int j = line.length() - 1; j >= firstDigitPointer; j--) {
-                    if (Character.isDigit(line.charAt(j))) {
-                        lastDigitPointer = j;
-                        break;
-                    }
-                }
-
-                // Combine the two digits to form a two-digit number
-                int twoDigitNum = Integer.parseInt(String.valueOf(line.charAt(firstDigitPointer)) + line.charAt(lastDigitPointer));
-
-                // Add the two-digit number to the sum
-                sum += twoDigitNum;
+                // Add the calibration value to the sum
+                sum += line.getCalibrationValue();
 
             });
 
-
-        } catch (URISyntaxException e) {
+        } catch (URISyntaxException | IOException e) {
             throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } finally {
-            return sum;
         }
+
+        return sum;
     }
 
     @Override
@@ -71,7 +89,7 @@ public class Day01Solver implements DaySolver {
         return 0;
     }
 
-    private List<String> fetchLines() throws URISyntaxException, IOException {
+    private List<String> fetchLinesText() throws URISyntaxException, IOException {
         return Files.readAllLines(Paths.get(getClass().getClassLoader().getResource(FILE_PATH).toURI()));
     }
 
